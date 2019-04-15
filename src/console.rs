2b5277ea82a8c8ed7a3a29aa0f5b5a4c1ec486fe
Stepan Kurennykh <kurennykhss@msk.bcs.ru>
2019-04-15 16:51:37 +0300

    change print_board error handling

--- src/console.rs
+++ src/console.rs
@@ -1,29 +1,29 @@
 use crossterm::{terminal, ClearType};
+use std::io;
 
-pub fn print_board(board: &Vec<i32>) {
+pub fn print_board(board: &Vec<i32>) -> io::Result<()> {
     let terminal = terminal();
 
-    terminal
-        .clear(ClearType::All)
-        .expect("Can't clear terminal!");
+    terminal.clear(ClearType::All)?;
 
     let board_side_size = (board.len() as f64).sqrt();
     if board_side_size.fract() != 0. {
-        panic!("Board width and lenght must be equal!");
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Board width and lenght must be equal!",
+        ));
     }
 
     let board_side_size = board_side_size as i16;
-    terminal
-        .set_size(board_side_size, board_side_size)
-        .expect("Can't set terminal size!");
+    terminal.set_size(board_side_size, board_side_size)?;
 
     for row in 0..board_side_size {
         let start_index = (row * board_side_size) as usize;
         for element in &board[start_index..start_index + board_side_size as usize] {
-            terminal
-                .write(format!("{: >5}", element))
-                .expect("Can't write to the terminal!");
+            terminal.write(format!("{: >5}", element))?;
         }
-        terminal.write("\n").expect("Can't write to the terminal!");
+        terminal.write("\n")?;
     }
+
+    Ok(())
 }
