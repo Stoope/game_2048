08c15fd7f2f86623d24299898f64bfb6d6f231e4
Stepan Kurennykh <kurennykhss@msk.bcs.ru>
2019-04-15 16:15:53 +0300

    add console mod

---
+++ src/console.rs
@@ -0,0 +1,29 @@
+use crossterm::{terminal, ClearType};
+
+pub fn print_board(board: &Vec<i32>) {
+    let terminal = terminal();
+
+    terminal
+        .clear(ClearType::All)
+        .expect("Can't clear terminal!");
+
+    let board_side_size = (board.len() as f64).sqrt();
+    if board_side_size.fract() != 0. {
+        panic!("Board width and lenght must be equal!");
+    }
+
+    let board_side_size = board_side_size as i16;
+    terminal
+        .set_size(board_side_size, board_side_size)
+        .expect("Can't set terminal size!");
+
+    for row in 0..board_side_size {
+        let start_index = (row * board_side_size) as usize;
+        terminal
+            .write(format!(
+                "{:?}\n",
+                &board[start_index..start_index + board_side_size as usize]
+            ))
+            .expect("Can't write to the terminal!");
+    }
+}
