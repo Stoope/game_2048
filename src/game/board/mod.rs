f3f1cce5e59222ddd1bbfd3f5e87e920fdc5d83e
Stepan Kurennykh <kurennykhss@msk.bcs.ru>
2019-04-15 18:50:19 +0300

    reorganize mods

---
+++ src/game/board/mod.rs
@@ -0,0 +1,30 @@
+use crossterm::{InputEvent, KeyEvent};
+mod console;
+
+pub struct Board {
+    board: Vec<i32>,
+}
+
+impl Board {
+    pub fn new(board_size: i16) -> Board {
+        let board: Vec<i32> = vec![0; board_size as usize];
+
+        Board { board }
+    }
+
+    pub fn perform_action(&self, key_event: &InputEvent) {
+        match key_event {
+            InputEvent::Keyboard(k) => match k {
+                KeyEvent::Up => {
+                    console::print_board(&self.board).expect("Error while processing board!");
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
+pub fn board(board_size: i16) -> Board {
+    Board::new(board_size)
+}
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,6 +1,7 @@
 use crossterm::{input, InputEvent, KeyEvent, RawScreen};
+mod board;
 
-fn check_quit(key_event: InputEvent) -> bool {
+fn check_quit(key_event: &InputEvent) -> bool {
     match key_event {
         InputEvent::Keyboard(k) => match k {
             KeyEvent::Char(c) => match c {
@@ -12,19 +13,11 @@ fn check_quit(key_event: InputEvent) -> bool {
         _ => false,
     }
 }
-fn process_board(key_event: InputEvent, board: &Vec<i32>) -> Option<Vec<i32>> {
-    match key_event {
-        InputEvent::Keyboard(k) => match k {
-            KeyEvent::Up => None,
-            _ => None,
-        },
-        _ => None,
-    }
-}
 
 pub fn start_game(board_size: i16) {
     if let Ok(_raw) = RawScreen::into_raw_mode() {
         let input = input();
+        let board = board::board(board_size);
 
         let mut sync_stdin = input.read_sync();
 
@@ -32,9 +25,10 @@ pub fn start_game(board_size: i16) {
             let event = sync_stdin.next();
 
             if let Some(key_event) = event {
-                if check_quit(key_event) {
+                if check_quit(&key_event) {
                     break;
                 } else {
+                    board.perform_action(&key_event);
                 }
             }
         }
